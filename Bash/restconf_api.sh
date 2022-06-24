88e9a8299f6d43cb72ee36b6e01f903666f6a7d6
NetAcad DEVASC <changeme@example.com>
2022-06-24 16:46:58 +0000

    bash test

---
+++ Bash/restconf_api.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+now=$(date)
+
+echo "$now"
+echo "START FIRST API CALL"
+echo "===================="
+echo "FIRST API CALL"
+echo 
+echo "Status code:$status_code_put"
+echo "===================="
+echo "SECOND API CALL"
+echo "===================="
+echo "Status code:$status_code_get"
+echo "Interfaces:$interfaces"
+echo ""
